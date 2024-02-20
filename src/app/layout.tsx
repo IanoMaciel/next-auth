54f1946482619b0436f15285c9a63ea48c4eb7d3
Iano Maciel <ianomaciel6385@gmail.com>
2024-02-20 08:52:17 -0400

    build: installed libraries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { AuthProvider } from '@/providers/auth'
 
 // components
 import Header from '@/components/header'
@@ -20,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
